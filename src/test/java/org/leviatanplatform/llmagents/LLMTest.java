314808794d75b98d907b75cbefb334f06d820df8
CiudadAlex <ciudad.alex@gmail.com>
2026-06-19 16:35:09 +0200

    LLM Agents

--- src/test/java/org/leviatanplatform/llmagents/LLMTest.java
+++ src/test/java/org/leviatanplatform/llmagents/LLMTest.java
@@ -7,7 +7,10 @@ public class LLMTest {
     public static void main(String[] args) throws IOException {
 
         LLM llm = new LLM();
-        llm.call("what is a black hole?");
+        String response = llm.call("what is a black hole?");
+
+        System.out.println("#########################################################");
+        System.out.println(response);
     }
 }
 
