660739f48e2cd9a28d55a2692d480d0777c33431
CiudadAlex <ciudad.alex@gmail.com>
2026-06-19 16:30:36 +0200

    LLM Agents

--- src/main/java/org/leviatanplatform/llmagents/LLM.java
+++ src/main/java/org/leviatanplatform/llmagents/LLM.java
@@ -3,11 +3,16 @@ package org.leviatanplatform.llmagents;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 public class LLM {
 
+    private static final String DEFAULT_MODEL = "ai/llama3.1:8B-Q4_K_M";
+
     private final String model;
+
+    public LLM() {
+        this(DEFAULT_MODEL);
+    }
 
     public LLM(String model) {
         this.model = model;
@@ -33,29 +38,3 @@ public class LLM {
         return baos.toByteArray();
     }
 }
-
-
-/*
-"""
-Caller for an LLM. Example call:
-
-docker model run ai/llama3.1:8B-Q4_K_M "what is a black hole"
-"""
-
-
-class LLM:
-
-    def __init__(self, model="ai/llama3.1:8B-Q4_K_M"):
-        self.model = model
-
-    def execute(self, text):
-        # Execute command and capture output
-        result = subprocess.run(
-            ['docker', 'model', 'run', self.model, text],
-            capture_output=True,
-            text=True,
-            encoding='utf-8'
-        )
-
-        return result.stdout
-*/
--- src/test/java/org/leviatanplatform/llmagents/LLMTest.java
+++ src/test/java/org/leviatanplatform/llmagents/LLMTest.java
@@ -1,4 +1,38 @@
 package org.leviatanplatform.llmagents;
 
+import java.io.IOException;
+
 public class LLMTest {
+
+    public static void main(String[] args) throws IOException {
+
+        LLM llm = new LLM();
+        llm.call("what is a black hole?");
+    }
 }
+
+
+/*
+"""
+Caller for an LLM. Example call:
+
+docker model run ai/llama3.1:8B-Q4_K_M "what is a black hole"
+"""
+
+
+class LLM:
+
+    def __init__(self, model="ai/llama3.1:8B-Q4_K_M"):
+        self.model = model
+
+    def execute(self, text):
+        # Execute command and capture output
+        result = subprocess.run(
+            ['docker', 'model', 'run', self.model, text],
+            capture_output=True,
+            text=True,
+            encoding='utf-8'
+        )
+
+        return result.stdout
+*/
