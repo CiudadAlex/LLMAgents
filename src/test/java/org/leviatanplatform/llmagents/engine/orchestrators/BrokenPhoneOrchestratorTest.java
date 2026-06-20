cdc89c96691afe43cadd628795b015c93a18f408
CiudadAlex <ciudad.alex@gmail.com>
2026-06-20 03:02:55 +0200

    LLM Agents

--- src/test/java/org/leviatanplatform/llmagents/engine/orchestrators/BrokenPhoneOrchestratorTest.java
+++ src/test/java/org/leviatanplatform/llmagents/engine/orchestrators/BrokenPhoneOrchestratorTest.java
@@ -7,7 +7,7 @@ class BrokenPhoneOrchestratorTest {
     public static void main(String[] args) throws IOException {
 
         BrokenPhoneOrchestrator brokenPhoneOrchestrator = new BrokenPhoneOrchestrator();
-        askAndPrintResponse(brokenPhoneOrchestrator,"Our Solar System takes roughly 225–250 million years to complete a single orbit around the center of the Milky Way. " +
+        askAndPrintResponse(brokenPhoneOrchestrator,"Our Solar System takes roughly 250 million years to complete a single orbit around the center of the Milky Way. " +
                 "The last time Earth was in its current position around the galaxy, dinosaurs had not yet appeared." +
                 "An even more remarkable detail: at the center of the Milky Way lies a supermassive black hole called Sagittarius A*, containing about 4 million times the mass of the Sun.");
     }
