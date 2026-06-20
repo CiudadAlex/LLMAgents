c2333d743c9bb3428b9f02196a2f1a372a983ff5
CiudadAlex <ciudad.alex@gmail.com>
2026-06-20 16:01:42 +0200

    LLM Agents

--- src/main/java/org/leviatanplatform/llmagents/engine/orchestrators/WerewolfGameOrchestrator.java
+++ src/main/java/org/leviatanplatform/llmagents/engine/orchestrators/WerewolfGameOrchestrator.java
@@ -1,4 +1,23 @@
 package org.leviatanplatform.llmagents.engine.orchestrators;
 
+import org.leviatanplatform.llmagents.engine.agents.*;
+
+import java.util.List;
+
 public class WerewolfGameOrchestrator {
+
+    private List<AbstractAgent> listAgents;
+
+    public WerewolfGameOrchestrator() {
+        this.listAgents = List.of(new PeasantAgent(), new PeasantAgent(), new WerewolfAgent(),
+                new PeasantAgent(), new PeasantAgent(), new WerewolfAgent(), new PeasantAgent());
+    }
+
+    public WerewolfGameOrchestrator(String model) {
+        this.listAgents = List.of(new PeasantAgent(model), new PeasantAgent(model), new WerewolfAgent(model),
+                new PeasantAgent(model), new PeasantAgent(model), new WerewolfAgent(model), new PeasantAgent(model));
+    }
+
+    // FIXME finish
+
 }
