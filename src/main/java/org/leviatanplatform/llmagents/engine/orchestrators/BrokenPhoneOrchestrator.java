5b6d26c99cd64686196a38a41426d7ab765e9ba1
CiudadAlex <ciudad.alex@gmail.com>
2026-06-20 02:51:19 +0200

    LLM Agents

--- src/main/java/org/leviatanplatform/llmagents/engine/orchestrators/BrokenPhoneOrchestrator.java
+++ src/main/java/org/leviatanplatform/llmagents/engine/orchestrators/BrokenPhoneOrchestrator.java
@@ -1,7 +1,23 @@
 package org.leviatanplatform.llmagents.engine.orchestrators;
 
+import org.leviatanplatform.llmagents.engine.agents.*;
+
+import java.util.List;
+
 public class BrokenPhoneOrchestrator {
 
+    private List<AbstractAgent> listAgents;
+
+    public BrokenPhoneOrchestrator() {
+        this.listAgents = List.of(new MathematicianAgent(), new PhylosopherAgent(), new PhysicistAgent(),
+                new DumbAgent(), new LittleKidAgent(), new SoftwareEngineerAgent(), new TeacherAgent());
+    }
+
+    public BrokenPhoneOrchestrator(String model) {
+        this.listAgents = List.of(new MathematicianAgent(model), new PhylosopherAgent(model), new PhysicistAgent(model),
+                new DumbAgent(model), new LittleKidAgent(model), new SoftwareEngineerAgent(model), new TeacherAgent(model));
+    }
+
     // FIXME finish
-    // DumbAgent, LittleKidAgent, MathematicianAgent, PhylosopherAgent, PhysicistAgent, SoftwareEngineerAgent, TeacherAgent
+
 }
---
+++ src/test/java/org/leviatanplatform/llmagents/engine/orchestrators/BrokenPhoneOrchestratorTest.java
@@ -0,0 +1,18 @@
+package org.leviatanplatform.llmagents.engine.orchestrators;
+
+import java.io.IOException;
+
+class BrokenPhoneOrchestratorTest {
+
+    public static void main(String[] args) throws IOException {
+
+        BrokenPhoneOrchestrator brokenPhoneOrchestrator = new BrokenPhoneOrchestrator();
+        askAndPrintResponse(brokenPhoneOrchestrator,"Our Solar System takes roughly 225–250 million years to complete a single orbit around the center of the Milky Way. " +
+                "The last time Earth was in its current position around the galaxy, dinosaurs had not yet appeared." +
+                "An even more remarkable detail: at the center of the Milky Way lies a supermassive black hole called Sagittarius A*, containing about 4 million times the mass of the Sun.");
+    }
+
+    private static void askAndPrintResponse(BrokenPhoneOrchestrator brokenPhoneOrchestrator, String inputText) throws IOException {
+        // FIXME finish
+    }
+}
